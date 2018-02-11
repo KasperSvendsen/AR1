fb43faf478894ad160f5c77f1e74e4efbe9d828d
waisti <20104517@post.au.dk>
2018-02-11 13:45:59 +0100

    explosion

--- Assets/Scripts/ButtonRay.cs
+++ Assets/Scripts/ButtonRay.cs
@@ -8,59 +8,87 @@ public class ButtonRay : MonoBehaviour
 
     private GameObject millennium;
     private GameObject interceptor;
+    public ParticleSystem explosion;
 
     private float rayLength;
+    private bool shoot;
     private Ray ray;
+    private Ray ray2;
     private Material material;
 
-    public Button shootButton;
-
+    private float leftCannon;
 
     // Use this for initialization
     void Start()
     {
         millennium = GameObject.Find("Millennium Falcon");
         interceptor = GameObject.Find("Interceptor");
- 
-        rayLength = 10;
 
-        Button btn = shootButton.GetComponent<Button>();
-        btn.GetComponentInChildren<Text>().text = "Shoot";
-        btn.onClick.AddListener(TaskOnClick);
+        shoot = false;
+        explosion.Pause();
+        rayLength = 10;
+        leftCannon = 0.07f;
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Local positions of cameras
+        Vector3 leftLocalPos = new Vector3((millennium.transform.position.x - leftCannon), millennium.transform.position.y, millennium.transform.position.z);
 
-        //Vector3 worldPos = millennium.transform.position + millennium.transform.rotation * millennium.transform.localPosition;
+        //Vector3 rightLocalPos =
 
+        //World positions of cameras
+        Vector3 leftWorldPos = millennium.transform.position + millennium.transform.rotation * leftLocalPos;
 
+        //center cannon into scene view
         ray.origin = millennium.transform.position; 
         ray.direction = millennium.transform.forward;
 
-        //draw into scene view
         Debug.DrawRay(ray.origin, ray.direction * rayLength);
 
-    }
+        //left and right front cannons into scene view
+        ray2.origin = leftWorldPos;
+        ray2.direction = millennium.transform.forward;
+
+        Debug.DrawRay(ray.origin, ray.direction * rayLength);
 
 
-    void TaskOnClick()
-    {
-        Debug.Log("You have clicked the button!");
-        Physics.Raycast(ray.origin, ray.direction * rayLength);
+        //shoot if space is pressed
+        if (Input.GetKeyDown("space"))
+        {
+            //draw line
+            shoot = true;
 
-
+            RaycastHit hit;
         
+            //check collision
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, rayLength))
+            {
+                //Play explosion at collision point
+                explosion.transform.position = hit.point;
+                explosion.Play();
+            }
+        }else
+        {
+            //stop drawing line
+            shoot = false;
+        }
     }
+
+
+ 
 
     void OnRenderObject()
     {
-        //draw into game view
-        if (material == null)
+        //draw into game view if space is pressed
+        if (shoot == true)
         {
-            material = new Material(Shader.Find("Hidden/Internal-Colored"));
+            if (material == null)
+            {
+                material = new Material(Shader.Find("Hidden/Internal-Colored"));
+            }
             material.SetPass(0);
 
             GL.Begin(GL.LINES);
@@ -69,5 +97,6 @@ public class ButtonRay : MonoBehaviour
             GL.Vertex(ray.origin + ray.direction * rayLength);
             GL.End();
         }
+        
     }
 }
